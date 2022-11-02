606670c52e8eb27e82f503e1e5893729a472a2eb
mahmood <mahmoodbhat3135@gmail.com>
2022-11-02 15:07:45 +0800

    added
    CLI T and R options

--- setup.py
+++ setup.py
@@ -4,15 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="prdc",
-    version="0.1",
-    author="NAVER Corp.",
-    description="Compute precision, recall, density, and coverage metrics "
-                "for two sets of vectors.",
+    name="prdc_cli",
+    version="0.0.1",
+    author="NAVER Corp., Mahmood Hussain",
+    description="Compute precision, recall, density, and coverage metrics for custom datasets",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/clovaai/prdc",
+    url="https://github.com/mahmood-hussain/prdc",
     packages=setuptools.find_packages(),
+        entry_points ={
+            'console_scripts': [
+                'prdc_cli = prdc.prdc_cli:main'
+            ]
+        },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -22,6 +26,10 @@ setuptools.setup(
         'numpy',
         'scikit-learn',
         'scipy',
-        'joblib'
+        'joblib',
+        'torch',
+        'torchvision',
+        'Pillow',
+        'tqdm',
     ],
 )
